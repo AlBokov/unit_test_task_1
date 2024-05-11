68aa42fee16ae12ca552a6edd49b8deae98be86a
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-05-11 23:32:54 +0300

    Проверка функции автотестами

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,8 @@
 package ru.netology;
 
+import ru.netology.service.CashbackHackService;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    CashbackHackService service = new CashbackHackService();
+
 }
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void CashBack1000() {
+        int actual = service.remain(1000);
+        int expected = 1000;
+
+
+        assertEquals(actual,expected);
+    }
+
+
+    @Test
+    public void CashBack500() {
+        int actual = service.remain(500);
+        int expected = 500;
+
+
+        assertEquals(actual,expected);
+    }
+
+
+    @Test
+    public void CashBack2000() {
+        int actual = service.remain(2000);
+        int expected = 1000;
+
+
+        assertEquals(actual,expected);
+    }
+
+
+
+}
